bd41579081190fd6f19e2c2dd5cdb01f125aeb8e
NastyAnanasty <angrybirds280700@gmail.com>
2021-10-14 16:11:24 +0400

    Порождающая матрица

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import numpy as np
 
 
+def X_matrix43():
+    xmatr43 = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 0], [0, 1, 1]])
+    return xmatr43
+
+
 def gen_matrix(n, k, d):
-    gen_matrix = np.zeros((k, n))
-    for i in range(0, gen_matrix.shape[0]):
-        for j in range(0, k):
-            if i == j:
-                gen_matrix[i][j] = 1
+    gen_matrix = np.eye(k)
+    Xmatr = X_matrix43()
+    gen_matrix = np.hstack((gen_matrix, Xmatr))
     return gen_matrix
 
 
 if __name__ == '__main__':
-    mat = gen_matrix(7, 3, 4)
+    mat = gen_matrix(7, 4, 3)
     print(mat)
