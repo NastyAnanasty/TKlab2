8d8e928b890d66c7895f3aca306794b3d36dfcc9
NastyAnanasty <angrybirds280700@gmail.com>
2021-10-14 15:51:27 +0400

    Единичная матрица

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import numpy as np
 
 def gen_matrix(n, k, d):
     gen_matrix = np.zeros((k, n))
+    for i in range(0, gen_matrix.shape[0]):
+        for j in range(0, k):
+            if i == j:
+                gen_matrix[i][j] = 1
     return gen_matrix
 
 
