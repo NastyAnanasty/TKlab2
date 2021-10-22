0dceb07d0aad024d38d31e14d46d246a8b60a8d7
NastyAnanasty <angrybirds280700@gmail.com>
2021-10-22 21:55:14 +0400

    Первая часть лабы, возможно 2.5 неправильно

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def check_matrix73(d):
 
 
 def create_word():
-    word = np.array([0, 1, 1, 1])
+    word = np.array([1, 0, 0, 1])
     return word
 
 
@@ -32,7 +32,7 @@ def sent_word():
 
 
 def take_word():
-    mistake = np.array([0, 0, 0, 0, 0, 0, 1])
+    mistake = np.array([0, 1, 0, 0, 0, 1, 0])
     word = (sent_word() + mistake) % 2
     return word
 
@@ -42,23 +42,54 @@ def syndrome():
     return syndrome
 
 
-def find_one_error():
+def find_one_error(take_word):
     print("Отправленное сообщение:", sent_word())
-    print("Полученное сообщение:", take_word())
-    result = np.dot(take_word(), check_matrix73(3)) % 2
+    print("Полученное сообщение:", take_word)
+    result = np.dot(take_word, check_matrix73(3)) % 2
     print(result)
     if np.all(result == 0):
-        print("Ошибок нет! Полученное сообщение:", take_word())
-        return take_word()
+        print("Исправленное сообщение:", take_word)
+        return take_word
     if np.any(result == 1):
-        for i in range(0, 8):
+        for i in range(0, 7):
             if np.all(result == check_matrix73(3)[i]):
                 fix = np.zeros(7)
                 fix[i] = 1
-                result = (fix + take_word()) % 2
-                print("Ошибка исправлена! Полученное сообщение:", result)
+                result = (fix + take_word) % 2
+                print("Исправленное сообщение:", result)
                 return result
 
 
+def find_two_error():
+    result = find_one_error(take_word())
+    print("Первая ошибка исправлена")
+    res = find_one_error(result)
+    return res
+
+
+def number_errors_and_fix():
+    number_mistake = abs(sent_word() - take_word())
+    k = 0
+    for i in range(0, 7):
+        if number_mistake[i] == 1:
+            k = k + 1
+    if k == 0:
+        print("Отправленное сообщение:", sent_word())
+        print("Полученное сообщение:", take_word())
+        print("Ошибок нет!")
+        return 0
+    if k == 1:
+        print("Одна ошибка")
+        find_one_error(take_word())
+        return 0
+    if k == 2:
+        print("Две ошибки")
+        find_two_error()
+        return 0
+    if k > 2:
+        print("Ошибок больше, чем две")
+        return 0
+
+
 if __name__ == '__main__':
-    word = find_one_error()
+    word = number_errors_and_fix()
